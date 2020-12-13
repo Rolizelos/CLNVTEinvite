c11a5484b769820f4372062c551affc329e3e4f2
Glitch (davet-botu-v12) <none>
2020-12-13 18:38:59 +0000

    🏕🍈 Checkpoint ./events/ready.js:16929997/67 ./ayarlar.json:16929997/156 ./komutlar/yardım.js:16929997/98

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
    client.user.setStatus('dnd') 
-   client.user.setActivity(`${prefix}yardım |  | ${client.guilds.cache.size} Sunucu`);
+   client.user.setActivity(`Code My life`);
    console.log(`[ BOT HAZIR ] - [ SUNUCULAR - ${client.guilds.cache.size} ] - [ KULLANICILAR - ${client.guilds.cache
           .reduce((a, b) => a + b.memberCount, 0)
           .toLocaleString()} ]`)
